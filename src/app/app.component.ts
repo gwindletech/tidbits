17a40b6320d9b1f196c71e8e392c6b1410bc4d74
Geoffrey Windle <gwindletech@gmail.com>
2025-03-04 22:04:23 +1000

    Add toolbar

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { TidbitCreateComponent } from './tidbit-create/tidbit-create.component';
 import { TidbitListComponent } from './tidbit-list/tidbit-list.component';
+import { ToolbarComponent } from './toolbar/toolbar.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [TidbitCreateComponent, TidbitListComponent],
+  imports: [TidbitCreateComponent, TidbitListComponent, ToolbarComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
---
+++ src/app/toolbar/toolbar.component.ts
@@ -0,0 +1,18 @@
+import {MatButtonModule} from '@angular/material/button';
+import {MatToolbarModule} from '@angular/material/toolbar';
+import {ChangeDetectionStrategy, Component} from '@angular/core';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import {MatIconModule} from '@angular/material/icon';
+import {MatInputModule} from '@angular/material/input';
+
+@Component({
+  selector: 'app-toolbar',
+  standalone: true,
+  imports: [MatToolbarModule, MatButtonModule, MatIconModule, MatFormFieldModule, MatInputModule],
+  templateUrl: './toolbar.component.html',
+  styleUrl: './toolbar.component.scss',
+  changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class ToolbarComponent {
+
+}
